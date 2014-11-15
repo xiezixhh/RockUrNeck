8b0884286dbfb8b484c8198ba6b159916ca27010
xhh261672 <563249383@qq.com>
2014-11-16 02:57:03 +0800

    db

---
+++ modules/handler.js
@@ -0,0 +1,69 @@
+var mongodb = require('./db')
+var MongoClient = require('mongodb').MongoClient
+
+function Handler(){
+
+}
+
+module.exports = Handler
+
+Handler.createCollection = function createCollection(callback){
+	MongoClient.connect("mongodb://localhost:27017/neckcare", {native_parser:true}, function (err, db){
+		if(err){
+			mongodb.close()
+			return callback(err, null);
+		}
+		db.createCollection('Records', function (err, collection){
+			if (err){
+				db.close()
+				return callback(err, null)
+			}else{
+				// db.collection('Records',{strict:true}, function (err, collection){
+				// 	if (err){
+				// 		db.close()
+				// 		return callback(err, null)
+				// 	}
+
+				// 	collection.insert(newRecord, {safe : true}, function (err,record){
+				// 		if (err){
+				// 			db.close()
+				// 			return callback(err, null)
+				// 		}else {
+				// 			db.close()
+
+				// 			return callback(null, record[0])
+				// 		}
+				// 	})
+				// })
+
+				db.close()
+
+			}
+		})
+	})
+}
+
+Handler.insertRecord = function insertRecord(newRecord, callback){
+	MongoClient.connect("mongodb://localhost:27017/neckcare", {native_parser:true}, function (err, db){
+		if(err){
+			mongodb.close()
+			return callback(err, null);
+		}
+		db.collection('Records',{strict:true}, function (err, collection){
+			if (err){
+				db.close()
+				return callback(err, null)
+			}
+
+			collection.insert(newRecord, {safe : true}, function (err,record){
+				if (err){
+					db.close()
+					return callback(err, null)
+				}else {
+					db.close()
+					return callback(null, record[0])
+				}
+			})
+		})
+	})	
+}
---
+++ setting.js
@@ -0,0 +1,5 @@
+module.exports = {
+	cookieSecret: 'neckcaredatabase',
+	db:'neckcare',
+	host:'localhost',
+};
