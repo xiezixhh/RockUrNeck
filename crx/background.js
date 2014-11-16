d8022e965f739c45de4335c6c8cfc511203c6593
lavende <gilesliu12@gmail.com>
2014-11-16 11:51:49 +0800

    final

--- crx/background.js
+++ crx/background.js
@@ -1,5 +1,9 @@
+var url
+
 function postDate() {
-    $.post('http://localhost:3000/', {date:new Date()}, function(ret) {
+    $.post('http://localhost:3000/', {
+        url : url
+    }, function(ret) {
         console.log(ret)
     })
 
@@ -8,7 +12,7 @@ function postDate() {
 function openRelaxTab() {
     chrome.tabs.create(
         {
-            url: 'http://localhost:3000/relax'
+            url: 'http://localhost:3000/'
         },
         function(tab) { 
             console.log ('open')
@@ -19,15 +23,18 @@ function openRelaxTab() {
 
 function getCurrentTab() {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      var urls = tabs[0].url.split('\/')
-      var url = urls[0] + '//' + urls[2]
-      console.log(url);
-
+        var urls = tabs[0].url.split('\/')
+        url = urls[0] + '//' + urls[2]
+        console.log(url);
     });
 }
 
+
 $(document).ready(function() {
-    // setInterval( postDate, 8000 )
-    setInterval(getCurrentTab, 3000)
+    // var d = new Date().getTime() / 1000.0
+
+    setInterval( postDate, 3000 )
+    setInterval( openRelaxTab, 10000000000 )
+    setInterval( getCurrentTab, 3000 )
 })
 
